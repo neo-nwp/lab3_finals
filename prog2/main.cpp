e9d2e56ffb5f78afcfa9674e76b6c5523a005583
neo-nwp <5089918@nwpolytech.ca>
2024-09-27 13:12:22 -0600

    2nd commit

--- prog2/main.cpp
+++ prog2/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    double x, y;
+
+    cout << "Enter value for x: ";
+    cin >> x;
+    cout << "Enter value for y: ";
+    cin >> y;
+
+    double *ptrX = &x;
+    double *ptrY = &y;
+
+    double sum = *ptrX + *ptrY;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "Sum = " << sum << endl;
+
     return 0;
 }
--- prog3/main.cpp
+++ prog3/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    using VEC = int*;
+    using MATRIX = VEC*;
+
+    int a = 5;
+    VEC v = &a;  // v points to a
+    MATRIX m = &v;  // m points to v
+
+    std::cout << "Value of a using v: " << *v << std::endl;
+    std::cout << "Value of a using m: " << **m << std::endl;
+
     return 0;
 }
